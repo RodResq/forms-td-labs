5ba7673acc56dcaf343d976626ebf2479c8d9a42
RodResq <rodrigoresque@yahoo.com.br>
2022-12-09 09:53:21 -0300

    updt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { ModelFormComponent } from './model-form/model-form.component';
+
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ModelFormComponent } from './model-form/model-form.component';
     ModelFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
