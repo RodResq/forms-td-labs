18f4e873ba8fb77f36e19429bafaebcc59e36d6e
RodResq <rodrigoresque@yahoo.com.br>
2022-12-09 09:45:14 -0300

    updt

---
+++ src/app/model-form/model-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'model-form',
+  templateUrl: './model-form.component.html',
+  styleUrls: ['./model-form.component.css']
+})
+export class ModelFormComponent implements OnInit {
+
+  langs: string[] = [
+    'English',
+    'French',
+    'German'
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
